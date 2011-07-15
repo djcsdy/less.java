5a56980c500634adeccf05963138a3ec8ddac46d
Daniel Cassidy <mail@danielcassidy.me.uk>
2011-07-15 22:17:38 +0100

    Wrap errors inside LessCompileException constructor, not with a special static method on LessCompiler.

--- src/net/noiseinstitute/less/LessCompileException.java
+++ src/net/noiseinstitute/less/LessCompileException.java
@@ -1,6 +1,7 @@
 package net.noiseinstitute.less;
 
 import org.mozilla.javascript.JavaScriptException;
+import org.mozilla.javascript.Scriptable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +17,16 @@ public class LessCompileException extends Exception {
     private String callExtract;
     private int column;
     private List<String> extract;
+
+    public LessCompileException (Scriptable error) {
+        super(getMessage(error));
+        extractInfo(error);
+    }
+
+    public LessCompileException (JavaScriptException cause) {
+        super(getMessage(cause), cause);
+        extractInfo(cause);
+    }
 
     public LessCompileException (String type, String message, String filename, int index, int line, int callLine,
             String callExtract, int column, List<String> extract, JavaScriptException cause) {
@@ -60,5 +71,93 @@ public class LessCompileException extends Exception {
 
     public List<String> getExtract () {
         return new ArrayList<String>(extract);
+    }
+
+    private static String getMessage (Scriptable error) {
+        Object message = error.get("message", error);
+        if (message instanceof String) {
+            return (String) message;
+        } else {
+            return null;
+        }
+    }
+
+    private static String getMessage (JavaScriptException cause) {
+        Object value = cause.getValue();
+        if (value instanceof Scriptable) {
+            String message = getMessage((Scriptable) value);
+            if (message != null) {
+                return message;
+            }
+        }
+
+        return null;
+    }
+
+    private void extractInfo (JavaScriptException cause) {
+        Object value = cause.getValue();
+        if (value instanceof Scriptable) {
+            extractInfo((Scriptable) value);
+        }
+    }
+
+    private void extractInfo (Scriptable error) {
+        Object typeObj = error.get("type", error);
+        Object filenameObj = error.get("filename", error);
+        Object indexObj = error.get("index", error);
+        Object lineObj = error.get("line", error);
+        Object callLineObj = error.get("callLine", error);
+        Object callExtractObj = error.get("callExtract", error);
+        Object columnObj = error.get("column", error);
+        Object extractObj = error.get("extract", error);
+
+        type = typeObj instanceof String
+                ? (String) typeObj
+                : null;
+
+        filename = filenameObj instanceof String
+                ? (String) filenameObj
+                : null;
+
+        if (indexObj instanceof Number) {
+            index = ((Number) indexObj).intValue();
+        } else {
+            index = -1;
+        }
+
+        if (lineObj instanceof Number) {
+            line = ((Number) lineObj).intValue();
+        } else {
+            line = -1;
+        }
+
+        if (callLineObj instanceof Number) {
+            callLine = ((Number) callLineObj).intValue();
+        } else {
+            callLine = -1;
+        }
+
+        callExtract = callExtractObj instanceof String
+                ? (String) callExtractObj
+                : null;
+
+        if (columnObj instanceof Number) {
+            column = ((Number) columnObj).intValue();
+        } else {
+            column = -1;
+        }
+
+        if (extractObj instanceof Scriptable) {
+            Scriptable scriptableExtract = (Scriptable) extractObj;
+            Object lengthObj = scriptableExtract.get("length", scriptableExtract);
+            if (lengthObj instanceof Number) {
+                int length = ((Number) lengthObj).intValue();
+                extract = new ArrayList<String>(length);
+                for (int i=0; i<length; ++i) {
+                    Object extractLineObj = scriptableExtract.get(i, scriptableExtract);
+                    extract.add(extractLineObj.toString());
+                }
+            }
+        }
     }
 }
--- src/net/noiseinstitute/less/LessCompiler.java
+++ src/net/noiseinstitute/less/LessCompiler.java
@@ -102,7 +102,7 @@ public class LessCompiler {
             };
             parse.call(context, globalScope, parser, parseArgs);
         } catch (JavaScriptException e) {
-            throw wrapAndThrow(e);
+            throw new LessCompileException(e);
         } finally {
             Context.exit();
         }
@@ -110,114 +110,7 @@ public class LessCompiler {
         if (error.get() == null) {
             return output.get();
         } else {
-            throw wrapAndThrow((Scriptable) error.get());
-        }
-    }
-
-    private static LessCompileException wrapAndThrow (Scriptable error) throws LessCompileException {
-        throw wrapAndThrow(error, null);
-    }
-
-    private static LessCompileException wrapAndThrow (Scriptable error, JavaScriptException cause)
-            throws LessCompileException {
-        Object typeObj = error.get("type", error);
-        Object messageObj = error.get("message", error);
-        Object filenameObj = error.get("filename", error);
-        Object indexObj = error.get("index", error);
-        Object lineObj = error.get("line", error);
-        Object callLineObj = error.get("callLine", error);
-        Object callExtractObj = error.get("callExtract", error);
-        Object columnObj = error.get("column", error);
-        Object extractObj = error.get("extract", error);
-
-        String type = typeObj instanceof String
-                ? (String) typeObj
-                : null;
-
-        String message = messageObj instanceof String
-                ? (String) messageObj
-                : null;
-
-        String filename = filenameObj instanceof String
-                ? (String) filenameObj
-                : null;
-
-        int index;
-        if (indexObj instanceof Number) {
-            index = ((Number) indexObj).intValue();
-        } else {
-            index = -1;
-        }
-
-        int line;
-        if (lineObj instanceof Number) {
-            line = ((Number) lineObj).intValue();
-        } else {
-            line = -1;
-        }
-
-        int callLine;
-        if (callLineObj instanceof Number) {
-            callLine = ((Number) callLineObj).intValue();
-        } else {
-            callLine = -1;
-        }
-
-        String callExtract = callExtractObj instanceof String
-                ? (String) callExtractObj
-                : null;
-
-        int column;
-        if (columnObj instanceof Number) {
-            column = ((Number) columnObj).intValue();
-        } else {
-            column = -1;
-        }
-
-        List<String> extract = null;
-        if (extractObj instanceof Scriptable) {
-            Scriptable scriptableExtract = (Scriptable) extractObj;
-            Object lengthObj = scriptableExtract.get("length", scriptableExtract);
-            if (lengthObj instanceof Number) {
-                int length = ((Number) lengthObj).intValue();
-                extract = new ArrayList<String>(length);
-                for (int i=0; i<length; ++i) {
-                    Object extractLineObj = scriptableExtract.get(i, scriptableExtract);
-                    extract.add(extractLineObj.toString());
-                }
-            }
-        }
-
-        throw new LessCompileException(
-                type, message, filename, index, line, callLine, callExtract, column, extract, cause);
-    }
-
-    private static LessCompileException wrapAndThrow (JavaScriptException cause) throws LessCompileException {
-        Object value = cause.getValue();
-        if (value instanceof Scriptable) {
-            Scriptable scriptableValue = (Scriptable) value;
-            Object typeObj = scriptableValue.get("type", scriptableValue);
-            Object messageObj = scriptableValue.get("message", scriptableValue);
-
-            String type = typeObj instanceof String
-                    ? (String) typeObj
-                    : null;
-
-            String message = messageObj instanceof String
-                    ? (String) messageObj
-                    : null;
-
-            if (type == null) {
-                if (message == null) {
-                    throw new LessCompileError(cause);
-                } else {
-                    throw new LessCompileError(message, cause);
-                }
-            } else {
-                throw wrapAndThrow(scriptableValue, cause);
-            }
-        } else {
-            throw new LessCompileError(cause);
+            throw new LessCompileException((Scriptable) error.get());
         }
     }
 }
